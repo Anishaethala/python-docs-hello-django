f10758ee92dd43c1f160201e15532fecc0701c35
Anishaethala <94163567+Anishaethala@users.noreply.github.com>
2021-11-12 06:13:34 -0800

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Anisha web application!")
+    return HttpResponse("Wishing you a very HAPPY BIRTHDAY TO YOU chef Blesson...... MAY THE LORD our GOD blesses you abundantly...........from: Always kopam thepinche, devil,irritator :)Ani")
+                                                                                   
